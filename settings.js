8d16681987cf2eb1c8650e3ad383acb237cb4545
Ayuchann <137247681+Ayuchann@users.noreply.github.com>
2023-10-17 23:24:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'Zeeoneofc'// yourname
 global.MONGO_DB_URI = "mongodb+srv://Miacantik:75KBVKqPHue3r1jE@cluster0.njkjca8.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "miamultidevice@gmail.com" //email
+global.email_password = "jpickamfifhmwscf" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
